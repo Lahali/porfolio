17662394c04cffc3f69dc7c9f68654bcaa99a972
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-04-14 12:45:39 +0200

    cleaned unused files

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -1,4 +1,3 @@
 export { default as dropon } from "./drop-on.png"
 export { default as rainbow } from "./rainbow-todo.png"
-export { default as waves } from "./layered-waves-haikei.png"
 export { default as budget } from "./budget.png"
