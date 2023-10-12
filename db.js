e79a4887db5692be3ba8f32fd65fa6f5f0d3eced
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-10-12 12:30:53 +0200

    added projects

--- db.js
+++ db.js
@@ -3,14 +3,6 @@ import { rainbow, dropon, budget, films } from "./public/assets/images"
 const projects = [
   {
     id: 1,
-    title: "Budget app",
-    description: "An app to create budgets for web services",
-    technologies: "React, Styled Components",
-    link: "https://budget-app-ivory.vercel.app/",
-    img: budget,
-  },
-  {
-    id: 2,
     title: "Where Was Filmed",
     description: "An app where the user can see and add movie locations",
     technologies: "React, Tailwind, Leaflet and Firebase",
@@ -18,8 +10,26 @@ const projects = [
     img: films,
   },
 
+  // {
+  //   id: 2,
+  //   title: "Knit Journal",
+  //   description: "An app where the user can add handcraft projects",
+  //   technologies: "React, Tailwind and Firebase",
+  //   link: "https://knittingjournal.vercel.app/",
+  //   img:
+  // },
+
   {
     id: 3,
+    title: "Budget app",
+    description: "An app to create budgets for web services",
+    technologies: "React, Styled Components",
+    link: "https://budget-app-ivory.vercel.app/",
+    img: budget,
+  },
+
+  {
+    id: 4,
     title: "Drop Zone",
     description: "A drop-on app",
     technologies: "ES6, CSS and HTML",
@@ -27,7 +37,7 @@ const projects = [
     img: dropon,
   },
   {
-    id: 4,
+    id: 5,
     title: "Rainbow to-do",
     description: "A To-Do app that creates a rainbow.",
     technologies: "React, Styled Components",
